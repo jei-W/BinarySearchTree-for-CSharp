025f9f26b57d066c40335682c351aece0cc79374
jei-W <gkdtkdv123@naver.com>
2019-02-13 00:31:00 +0900

    BinarySearchTree 구현 == *이진탐색트리의 Node와 Insert 구현

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        //1. 처음 rootnode는 null이다.
+
+        //3. 삽입할 때, 트리의 재배치는 하지 않는다.
+        //4. 삭제할 때만 트리의 재배치를 수행한다.
+        //5. 트리안에 데이터를 전체 출력할 수 있어야 한다
+
+        Node rootNode = null;
+        Node node;
+
+        void Insert( int value )
+        {
+            Node valueNode = new Node(value);
+
+            //2. 첫 삽입에 root node가 null이면 루트 노드를 생성한다
+            if( rootNode == null )
+            {
+                rootNode = valueNode;
+            }
+            else
+            {
+                node = rootNode;
+                CheckPosition();
+            }
+
+            void CheckPosition()
+            {
+                if( valueNode < node )
+                {
+                    Node parents = node;
+                    node = node.GetLeft();
+                    node.SetParents(node);
+
+                    if( node == null )
+                    {
+                        node = valueNode;
+                    }
+                    else
+                    {
+                        CheckPosition();
+                    }
+                }
+
+                else if( valueNode > node )
+                {
+                    node.SetParents(node);
+                    node = node.GetRight();
+
+                    if( node == null )
+                    {
+                        node = valueNode;
+                    }
+                    else
+                    {
+                        CheckPosition();
+                    }
+                }
+            }
+        }
+
+        void Remove( int value )
+        {
+
+        }
+
+        void Print()
+        {
+
+        }
+
+        void Find( int value )
+        {
+
+        }
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        Node parents = null; //왜 nullable연산자를 쓰지 않고 널이 가능할까?
+        Node left = null;
+        Node right = null;       
+
+        int value;
+
+        public Node()
+        {
+        }
+
+        public Node( int value )
+        {
+            this.value = value;
+        }
+
+        public Node GetLeft()
+        {
+            return left;
+        }
+
+        public Node GetRight()
+        {
+            return right;
+        }
+
+        public void SetLeft( Node node )
+        {
+            left = node;
+        }
+
+        public void SetRight( Node node )
+        {
+            right = node;
+        }
+
+        public void SetParents(Node node)
+        {
+            parents = node;
+            return;
+        }
+
+        // operator의 오버로딩 : 연산 가능하게 추가
+        #region operator
+        public static bool operator <( Node lhs, Node rhs )
+        {
+            return lhs.value < rhs.value;
+        }
+
+        public static bool operator >( Node lhs, Node rhs )
+        {
+            return lhs.value > rhs.value;
+        }
+
+        public static bool operator <( Node lhs, int rhs )
+        {
+            return lhs.value < rhs;
+        }
+
+        public static bool operator >( Node lhs, int rhs )
+        {
+            return lhs.value > rhs;
+        }
+        #endregion
+    }
+}
